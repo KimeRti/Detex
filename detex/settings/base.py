8d7cae4778b0141e2e359cdc967d8af6bc07b908
Ahmet <merterli_ahmet18@hotmail.com>
2023-05-26 17:30:45 +0300

    fix

--- detex/settings/base.py
+++ detex/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$=hsly6gc_zq)sq*5=vqy#8)ju9=6z=p#qv!#f2zry2(dck5@(
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
