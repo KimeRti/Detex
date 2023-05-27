0d573c24f5babd2398b802a129c5dd40bd616bc1
Ahmet <merterli_ahmet18@hotmail.com>
2023-05-27 11:40:12 +0300

    Update production.py

--- detex/settings/production.py
+++ detex/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['detextools.com', 'www.detextools.com']
+ALLOWED_HOSTS = ['45.147.46.199', 'detextools.com', 'www.detextools.com']
 
 DATABASES = {
     'default': {
@@ -22,7 +22,7 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
